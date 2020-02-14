f40ba7eaf62507b5d41318ff54f4b58671bfa26e
kj2386 <kj2386@gmail.com>
2020-02-14 15:16:14 -0700

    exercise routes done

---
+++ controllers/exercise.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const Exercise = require('../db/models/Exercise');
+
+router.get('/', (req, res) => {
+  Exercise.find({}).then(exercises => res.json(exercises));
+});
+
+router.get('/:id', (req, res) => {
+  Exercise.find({ _id: req.params.id }).then(exercise => {
+    res.json(exercise);
+  });
+});
+
+router.post('/', (req, res) => {
+  Exercise.create(req.body).then(exercise => res.json(exercise));
+});
+
+router.put('/:id', (req, res) => {
+  Exercise.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true
+  }).then(exercise => res.json(exercise));
+});
+
+router.delete('/:id', (req, res) => {
+  Exercise.findOneAndRemove({ _id: req.params.id }).then(exercise => {
+    res.json(exercise);
+  });
+});
+
+module.exports = router;
--- controllers/workout.js
+++ controllers/workout.js
@@ -1,1 +1,5 @@
 const express = require('express');
+const router = express.Router();
+const Workout = require('../db/models/Workout');
+
+module.exports = router;
