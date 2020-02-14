4c21f95d4b5ebdb5e1c26ce7bc5d8402fbb781ea
kj2386 <kj2386@gmail.com>
2020-02-14 10:16:59 -0700

    connected to db

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+  console.log(`✅ PORT: ${app.get('port')} 🌟`);
+});
